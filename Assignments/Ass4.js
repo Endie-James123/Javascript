05611ea58277e07eb3a724a8dcc6c3d2c3a45d28
Endie-James123 <ndopnnoabasi@gmail.com>
2023-12-10 20:43:31 +0100

    Added another time assignment

--- Assignments/Ass4.js
+++ Assignments/Ass4.js
@@ -1,18 +1,19 @@
 let yearOfBirth = Number(prompt("what is your birthyear"));
+const currentYear =2023
 if (yearOfBirth >= 1922 && yearOfBirth <= 1927) {
-  console.log("You are a WWII");
+  console.log(`You are ${currentYear - yearOfBirth} years old`);
 } else if (yearOfBirth >= 1928 && yearOfBirth <= 1945) {
-  console.log("You belong to the Post War Generation.");
+  console.log(`You are ${currentYear - yearOfBirth} years old so you belong to the Post War Generation.`);
 } else if (yearOfBirth >= 1946 && yearOfBirth <= 1954) {
-  console.log("You belong to the Boomers I* Generation.");
+  console.log(`You are ${currentYear - yearOfBirth} years old so you belong to the Boomers I* Generation.`);
 } else if (yearOfBirth >= 1955 && yearOfBirth <= 1964) {
-  console.log("You belong to the boomers II (a/k/a Generation Jones)*.");
+  console.log(`You are ${currentYear - yearOfBirth} years old so you belong to the boomers II (a/k/a Generation Jones)*.`);
 } else if (yearOfBirth >= 1965 && yearOfBirth <= 1980) {
-  console.log("You belong to the Gen X Generation.");
+  console.log(`You are ${currentYear - yearOfBirth}years old so you belong to the Gen X Generation.`);
 } else if (yearOfBirth >= 1981 && yearOfBirth <= 1996) {
-  console.log("You belong to the Millennials.");
+  console.log(`You are ${currentYear - yearOfBirth}years old so you belong to the Millennials.`);
 } else if (yearOfBirth >= 1997 && yearOfBirth <= 2012) {
-  console.log("You belong to the Gen Z Generation.");
+  console.log(`You ${currentYear - yearOfBirth}years old so you belong to the Gen Z Generation.`);
 } else {
   console.log(`You're a very small boy of ${2023 - yearOfBirth}`);
 }
